eb59c0f543a0a53ba71d530281578280675f1d0a
sasaki <sasaki@newcom07.jp>
2019-08-21 17:00:33 +0900

    特になし

--- Practice/heihoukon.py
+++ Practice/heihoukon.py
@@ -1,15 +1,25 @@
 class Main:
-    def __init__(self,num):
+    def __init__(self, num, deci):
         self.num = num
+        self.deci = deci
 
     def Hei(self):
         for i in range(1, self.num+1):
             if self.num > (self.num / i) ** 2:
                 break
-        print(i-1)
+                
+        root = i - 1 + 0.1 ** self.deci
+        
+        for i in range(self.deci):
+            while root ** 2 < self.num:
+                root += 0.1 ** (i + 1)
+            else:
+                root -= 0.1 ** (i + 1)
+                
+        print(round(root, self.deci))
+        
         
 num = int(input('数値:'))
-Main(num).Hei()
-#for j in range(i * 10,  (i - 1) * 10, -1):
-#    if x < (x / (j / 10)) ** 2:
-#        break
+deci = int(input('桁数:'))
+
+Main(num,deci).Hei()
